1f537c345c96dc8a6fdac2cdf1f252f242b5bdbc
Newp <mynewp@gmail.com>
2021-05-23 18:58:13 +0900

    윈폼 gui 추가

---
+++ src/Npgg.PsdToSpineGUI/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Npgg.PsdToSpineGUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+            string boneMapPath = "boneMap.json";
+
+            var boneMap = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(boneMapPath));
+            var result = Npgg.PsdToSpine.Converter.Convert("sample1.psd", boneMap);
+
+            
+        }
+    }
+}
